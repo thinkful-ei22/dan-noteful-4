2ff3244d7601e9133bd095caf26210019124e606
Daniyar Salikhov <salihovdr@gmail.com>
2018-07-16 15:28:42 -0400

    Add passport/local.js. Update models/user.js

---
+++ passport/local.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { Strategy: LocalStrategy } = require('passport-local');
+const User = require('../models/user');
+
+const LocalStrategy = new LocalStrategy((username, password, done) => {
+  let user;
+  User.findOne({ username })
+    .then(results => {
+      if(!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username',
+          location: 'username'
+        });
+      }
+      const isValid = user.validatePassword(password);
+      if(!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect password',
+          location: 'password'
+        });
+      }
+      return done(null, user);
+    })
+    .catch(err => {
+      if(err.reason === 'LoginError') {
+        return done(null, false);
+      }
+      return done(err);
+    })
+})

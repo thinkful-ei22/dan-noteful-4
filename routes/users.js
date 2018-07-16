e40fcf080f041553f9aee1615720ae9e90796c1e
Daniyar Salikhov <salihovdr@gmail.com>
2018-07-16 16:50:12 -0400

    Add field validation

--- routes/users.js
+++ routes/users.js
@@ -22,14 +22,57 @@ router.post('/', (req, res, next) => {
       const missingField = requiredFields.find(field => !(field in req.body));
 
       if (missingField) {
-        return res.status(422).json({
-          code: 422,
-          reason: 'ValidationError',
-          message: 'Missing field',
-          location: missingField
-        });
+        const err = new Error(`Missing '${missingField}' in request body`);
+        err.status = 422;
+        return next(err);
       }
-      
+
+      const stringFields = ['fullname', 'username', 'password'];
+      const nonStringField = stringFields.find(field => field in req.body && typeof req.body[field] !== 'string');
+
+      if (nonStringField) {
+        const err = new Error(`'${nonStringField}': Incorrect field type: expected string`);
+        err.status = 422;
+        next(err);
+      }
+
+      const explicityTrimmedFields = ['username', 'password'];
+      const nonTrimmedField = explicityTrimmedFields.find(field => req.body[field].trim() !== req.body[field]);
+
+      if (nonTrimmedField) {
+        const err = new Error(`'${nonTrimmedField}' cannot start or end with whitespace`);
+        err.status = 422;
+        next(err);
+      }
+
+      const sizedFields = {
+        username: {
+          min: 1
+        },
+        password: {
+          min: 8,
+          max: 72
+        }
+      };
+
+      const tooSmallField = Object.keys(sizedFields).find(field => 'min' in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min);
+
+      const tooLargeField = Object.keys(sizedFields).find(field => 'max' in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max);
+
+      if (tooSmallField) {
+        const err = new Error(`'${tooSmallField}' must be at least ${sizedFields[tooSmallField]
+          .min} characters long`);
+        err.status = 422;
+        next(err);
+      }
+
+      if (tooLargeField) {
+        const err = new Error(`'${tooLargeField}' must be at most ${sizedFields[tooLargeField]
+          .max} characters long`);
+        err.status = 422;
+        next(err);
+      }
+
       return User.create(newUser);
     })
     .then(result => {
